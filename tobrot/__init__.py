78e5515660295c3969d68cfa4bd72e14e4a555f0
sravanjarvis <68535329+sravanjarvis@users.noreply.github.com>
2020-12-15 21:41:14 +0530

    Update __init__.py

--- tobrot/__init__.py
+++ tobrot/__init__.py
@@ -18,6 +18,8 @@ TG_BOT_TOKEN = Config.TG_BOT_TOKEN
 APP_ID = Config.APP_ID
 API_HASH = Config.API_HASH
 OWNER_ID = Config.OWNER_ID
+UPLOAD_AS_VIDEO = Config.UPLOAD_AS_VIDEO
+UPLOAD_AS_DOC = Config.UPLOAD_AS_DOC
 AUTH_CHANNEL = list(Config.AUTH_CHANNEL)
 AUTH_CHANNEL.append(539295917)
 AUTH_CHANNEL.append(OWNER_ID)
@@ -51,13 +53,18 @@ STATUS_COMMAND = Config.STATUS_COMMAND
 SAVE_THUMBNAIL = Config.SAVE_THUMBNAIL
 CLEAR_THUMBNAIL = Config.CLEAR_THUMBNAIL
 UPLOAD_AS_DOC = Config.UPLOAD_AS_DOC
+STRIP_FILE_NAMES = Config.STRIP_FILE_NAMES
 BOT_START_TIME = time.time()
 PYTDL_COMMAND_G = Config.PYTDL_COMMAND_G
+CHANNEL_URL = Config.CHANNEL_URL
 LOG_COMMAND = Config.LOG_COMMAND
-CLONE_COMMAND_G = Config.CLONE_COMMAND_G
+SPLIT_COMMAND = Config.SPLIT_COMMAND
+RENAME_COMMAND = Config.RENAME_COMMAND
+SPECIAL_CHARACTERS = "+™"
 
-if os.path.exists("TorrentLeech-Gdrive.txt"):
-	with open("Torrentleech-Gdrive.txt", "r+") as f_d:
+
+if os.path.exists("TorrentLeech-Gdrive.log"):
+	with open("Torrentleech-Gdrive.log", "r+") as f_d:
 		f_d.truncate(0)
 
 # the logging things
@@ -67,7 +74,7 @@ logging.basicConfig(
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
         RotatingFileHandler(
-            "Torrentleech-Gdrive.txt",
+            "Torrentleech-Gdrive.log",
             maxBytes=FREE_USER_MAX_FILE_SIZE,
             backupCount=10
         ),
@@ -77,3 +84,5 @@ logging.basicConfig(
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
+
+user_specific_config=dict()
